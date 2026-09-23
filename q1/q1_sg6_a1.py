a63ff7c51a88d6b0c0731af387df84d3507a4e06
iyayayahh <amcabanayan2030@clc.pshs.edu.ph>
2026-09-24 07:09:46 +0800

    Add Lab class and assign lab to Technician

--- q1/q1_sg6_a1.py
+++ q1/q1_sg6_a1.py
@@ -4,9 +4,11 @@ class Technician:
         self.assigned_lab=None
     def assign_lab (self, lab_obj):
         self.assigned_lab=lab_obj
+        
 class Lab:
     def __init__ (self, room_number):
         self.room_number=room_number
+
 
 mr_cruz=Technician("Mr. Cruz")
 chem_lab=Lab("216")
